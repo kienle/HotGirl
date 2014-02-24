44d19b6b3fbfad8a5f6e56f15545ceb0e6677a15
Kien Le <kienctlm123@gmail.com>
2014-02-24 12:31:32 +0700

    add

---
+++ src/com/kienle/famous/people/hotgirl/helper/Config.java
@@ -0,0 +1,9 @@
+package com.kienle.famous.people.hotgirl.helper;
+
+import android.os.Environment;
+
+public class Config {
+    public static final String DATA_ZIP_FILE = "data.zip";
+    public static final String APP_FOLDER = Environment.getExternalStorageDirectory().getPath() + "/HotGirl";
+    public static final String FOLDER_IMAGE = APP_FOLDER + "/hot_girl";
+}
